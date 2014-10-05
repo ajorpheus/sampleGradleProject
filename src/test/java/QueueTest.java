3b7ee67a4088cc9f9352db3c7efa7dd0d78586ba
Quinn Liu <quinnliu@vt.edu>
2014-10-05 17:43:49 -0400

    initial skeleton is compiling

---
+++ src/test/java/QueueTest.java
@@ -0,0 +1,14 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by Huanqing on 10/5/2014.
+ */
+public class QueueTest extends TestCase {
+    public void setUp() {
+        System.out.println("hi");
+    }
+
+    public void test_enqueue() {
+        assertEquals(true, true);
+    }
+}
